b3a74a082da52817a0c03b4a4c112f8b21c2b44c
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-12-14 23:04:15 +0700

    google user authentication firebase

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:chatkuy/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -8,21 +9,31 @@ class AuthController extends GetxController {
 
   GoogleSignIn _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
+  UserCredential? userCredential;
 
   Future<void> login() async {
     try {
       await _googleSignIn.signOut();
       await _googleSignIn.signIn().then((value) => _currentUser = value);
+      final isSignIn = await _googleSignIn.isSignedIn();
 
-      await _googleSignIn.isSignedIn().then((value) {
-        if (value) {
-          print(_currentUser);
-          isAuth.value = true;
-          Get.offAllNamed(Routes.HOME);
-        } else {
-          print("Tidak berhasil login");
-        }
-      });
+      if (isSignIn) {
+        final googleAuth = await _currentUser!.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) => userCredential = value);
+
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        print("Tidak berhasil login");
+      }
     } catch (error) {
       print(error);
     }
