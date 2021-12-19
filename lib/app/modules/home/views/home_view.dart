5db85d1496141c4d792b508e5018be120ea35738
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-12-19 15:40:02 +0700

    realtime chat with stream builder and streamchats

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,78 +1,116 @@
+import 'package:chatkuy/app/controllers/auth_controller.dart';
 import 'package:chatkuy/app/routes/app_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  final List<Widget> myChats = List.generate(
-    20,
-    (index) => ListTile(
-        onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundColor: Colors.black26,
-          child: Image.asset(
-            "assets/logo/noimage.png",
-            fit: BoxFit.cover,
-          ),
-        ),
-        title: Text(
-          "Orang ke ${index + 1}",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        subtitle: Text("chatnya orang ke ${index + 1}",
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-            )),
-        trailing: Chip(label: Text("3"))),
-  ).reversed.toList();
+  final authC = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Material(
-          elevation: 3,
-          child: Container(
-            margin: EdgeInsets.only(top: context.mediaQueryPadding.top),
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Chatkuy",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                InkWell(
-                  onTap: () => Get.toNamed(Routes.PROFILE),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Icon(
-                      Icons.person,
-                      size: 35,
-                    ),
+      body: Column(
+        children: [
+          Material(
+            elevation: 5,
+            child: Container(
+              margin: EdgeInsets.only(top: context.mediaQueryPadding.top),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black38,
                   ),
                 ),
-              ],
+              ),
+              padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Chats",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.red[900],
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () => Get.toNamed(Routes.PROFILE),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.person,
+                          size: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index]),
-        ),
-      ]),
+          Expanded(
+            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: controller.chatsStream(authC.user.value.email!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  var allChats = (snapshot.data!.data()
+                      as Map<String, dynamic>)["chats"] as List;
+                  return ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: allChats.length,
+                    itemBuilder: (context, index) => ListTile(
+                      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black26,
+                        child: Image.asset(
+                          "assets/logo/noimage.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text(
+                        "Orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "status orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      trailing: Chip(
+                        label: Text("3"),
+                      ),
+                    ),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Get.toNamed(Routes.SEARCH),
-        child: Icon(Icons.message_rounded),
+        child: Icon(
+          Icons.search,
+          size: 30,
+        ),
+        backgroundColor: Colors.red[900],
       ),
     );
   }
