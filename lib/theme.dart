92f04c86ed24815b4bcf1128d0d83a52944365f5
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-10 18:16:42 +0700

    darkThemeData

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:chatkuy/constants.dart';
 import 'package:flutter/material.dart';
 
+final appBarTheme = AppBarTheme(centerTitle: false, elevation: 0);
+
 ThemeData lightThemeData(BuildContext context) {
   return ThemeData.light().copyWith(
     primaryColor: kPrimaryColor,
@@ -8,6 +10,17 @@ ThemeData lightThemeData(BuildContext context) {
     appBarTheme: appBarTheme,
     iconTheme: IconThemeData(color: kContentColorLightTheme),
     colorScheme: ColorScheme.light(
+        primary: kPrimaryColor, secondary: kSecondaryColor, error: kErrorColor),
+  );
+}
+
+ThemeData darkThemeData(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kContentColorLightTheme,
+    appBarTheme: appBarTheme,
+    iconTheme: IconThemeData(color: kContentColorDarkTheme),
+    colorScheme: ColorScheme.dark(
         primary: kPrimaryColor, secondary: kSecondaryColor, error: kErrorColor),
   );
 }
