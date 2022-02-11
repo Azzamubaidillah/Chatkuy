93ca8796f2e3e330b83cc201965a544b18b6ee86
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-11 11:05:44 +0700

    error mediaquery ancestor

--- lib/app/utils/splash_screen.dart
+++ lib/app/utils/splash_screen.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Center(
-          child: Container(
-        width: Get.width * 0.7,
-        height: Get.width * 0.7,
-        child: Lottie.asset("assets/lottie/hello.json"),
-      )),
-    ));
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Image.asset("assets/logo/logo_light.png"),
+        ),
+      ),
+    );
   }
 }
