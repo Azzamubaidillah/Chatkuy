504aff6ea4696b20ac5ee9d7bd7b39a9283648ee
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-02-12 12:52:30 +0700

    create info page

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -100,7 +100,7 @@ class ProfileView extends GetView<ProfileController> {
                     isDarkMode: isDarkMode,
                     text: "Info",
                     icon: Icon(LineAwesomeIcons.question_circle),
-                    route: () => Get.toNamed(Routes.UPDATE_STATUS),
+                    route: () => Get.toNamed(Routes.INFO),
                   ),
                   ProfileListView(
                     authC: authC,
